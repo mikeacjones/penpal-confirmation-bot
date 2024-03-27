b12cef6b12232b909ad22d8dd4bb9fd061cc96da
Michael Jones <25686778+mikeacjones@users.noreply.github.com>
2024-03-27 18:08:13 -0400

    Added test case for outage recovery rather than just startup

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -4,6 +4,7 @@ from betamax_helpers import use_recorder
 import main
 from main import handle_catchup
 from praw import Reddit
+from datetime import datetime, timezone
 
 
 @use_recorder
@@ -11,3 +12,10 @@ def test_handle_catchup(recorder: Betamax, settings: Settings, bot: Reddit):
     main.SETTINGS = settings
     main.BOT = bot
     handle_catchup()
+
+
+@use_recorder
+def test_handle_catchup_with_outage(recorder: Betamax, settings: Settings, bot: Reddit):
+    main.SETTINGS = settings
+    main.BOT = bot
+    handle_catchup(datetime.now(timezone.utc))
